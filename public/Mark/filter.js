c6dd2cecd5ad55bdabd2e36b018b49134f978451
Mark Bout <markhendrikbout@gmail.com>
2020-07-19 20:34:06 +0200

    NL design Filters toe gevoegd

---
+++ public/Mark/filter.js
@@ -0,0 +1,54 @@
+function mbochanged() {
+    if($('#chkbx-compact-6').not((':checked'))){
+        $('.MBO').hide();
+    }
+    if($('#chkbx-compact-6').is((':checked'))){
+        $('.MBO').show();
+    }
+}
+
+function vwochanged() {
+    if($('#chkbx-compact-5').not((':checked'))){
+        $('.VWO').hide();
+    }
+    if($('#chkbx-compact-5').is((':checked'))){
+        $('.VWO').show();
+    }
+}
+
+function havochanged() {
+    if($('#chkbx-compact-4').not((':checked'))){
+        $('.HAVO').hide();
+    }
+    if($('#chkbx-compact-4').is((':checked'))){
+        $('.HAVO').show();
+    }
+}
+
+function zorgchanged() {
+    if($('#chkbx-compact-3').not((':checked'))){
+        $('.ZORG').hide();
+    }
+    if($('#chkbx-compact-3').is((':checked'))){
+        $('.ZORG').show();
+    }
+}
+
+function itchanged() {
+    if($('#chkbx-compact-2').not((':checked'))){
+        $('.ICT').hide();
+    }
+    if($('#chkbx-compact-2').is((':checked'))){
+        $('.ICT').show();
+    }
+}
+
+function aochanged() {
+    if($('#chkbx-compact-1').not((':checked'))){
+        $('.SOFTWARE-ONTWIKKELAAR').hide();
+    }
+    if($('#chkbx-compact-1').is((':checked'))){
+        $('.SOFTWARE-ONTWIKKELAAR').show();
+    }
+}
+
